9e844c6c7816c386fb2060372c8913a2951d2d0f
X023840 <X023840@USE2DEVP5773.ead.thomsonreuters.com>
2022-10-13 15:44:42 +0530

    8.15 Sprint 1 cases

--- LYNX_Automation/src/main/java/BasePackage/LYNXBase.java
+++ LYNX_Automation/src/main/java/BasePackage/LYNXBase.java
@@ -1,5 +1,6 @@
 package BasePackage;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Properties;
 
 import org.sikuli.script.App;
@@ -60,28 +61,77 @@ public class LYNXBase {
 	
 	public static void RelaunchFWTab(ExtentTest test) throws FindFailed, InterruptedException {
 		
-		if(s.exists(GetProperty("Fastwiretab"))!=null) {
-			while (s.exists(GetProperty("Fastwiretab"))!=null) {
-				if (s.exists(GetProperty("FWTabClose"))!=null) {
-					s.click(GetProperty("FWTabClose"));
+		 if(s.exists(GetProperty("LYNXEDITORLOGO"))!=null) {
+				if(s.exists(GetProperty("Fastwiretab"))!=null) {
+					while (s.exists(GetProperty("Fastwiretab"))!=null) {
+						if (s.exists(GetProperty("FWTabClose"))!=null) {
+							s.click(GetProperty("FWTabClose"));
+						}
+						if (s.exists(GetProperty("FWTabCloseunfocused")	)!=null) {
+							s.click(GetProperty("FWTabCloseunfocused"));
+						}
+					}
 				}
-				if (s.exists(GetProperty("FWTabCloseunfocused")	)!=null) {
-					s.click(GetProperty("FWTabCloseunfocused"));
+			    s.keyDown(Key.CTRL);
+				s.keyDown(Key.SHIFT);
+				s.type("F");
+				s.keyUp(Key.CTRL);
+				s.keyUp(Key.SHIFT);
+				if(s.wait(GetProperty("Fastwiretab"),5)!=null ) {
+					test.pass("Successfully opened new Fastwire tab");
 				}
-			}
-		}
-	    s.keyDown(Key.CTRL);
-		s.keyDown(Key.SHIFT);
-		s.type("F");
-		s.keyUp(Key.CTRL);
-		s.keyUp(Key.SHIFT);
-		if(s.wait(GetProperty("Fastwiretab"),5)!=null ) {
-			test.pass("Successfully opened new Fastwire tab");
-		}
-		else {
-			test.fail("Unable to relaunch Fastwire tab");
-		}
-		Thread.sleep(10000);
+				else {
+					test.fail("Unable to relaunch Fastwire tab");
+				}
+				Thread.sleep(10000);
+		 }
+		 else {
+			 	Runtime runtime = Runtime.getRuntime();     //getting Runtime object
+				try
+		        {	runtime.exec("taskkill /F /IM LYNX.exe");
+		            Thread.sleep(2000);
+		            lynxapp.open();
+				}
+		        catch (IOException e)
+		        {
+		            e.printStackTrace();
+		        }
+				Thread.sleep(10000);
+				if (s.wait(GetProperty("iPass"),30) != null) {
+					
+					test.pass("Launched LYNX Application");
+					s.type(GetProperty("iPass"), LYNXProp.getProperty("tPass"));
+					test.pass("Provided User name and Password");
+				} else {
+					test.fail("Password field doesnot exist");
+				}
+				if (s.exists(GetProperty("iSignOn")) != null) {
+					s.find(GetProperty("iSignOn")).click();
+					test.pass("Clicked on Sign on button");
+				} else {
+					test.fail("Sign on button doesnot exist");
+				}
+				s.wait(GetProperty("Home"),50);
+				Thread.sleep(5000);
+				if(s.exists(GetProperty("Home"))!=null ) {
+					test.pass("Successfully logged into Fastwire");
+				}
+				else {
+					test.fail("Unable to login to Fastwire");
+				}
+				s.keyDown(Key.CTRL);
+				s.keyDown(Key.SHIFT);
+				s.type("F");
+				s.keyUp(Key.CTRL);
+				s.keyUp(Key.SHIFT);
+				if(s.wait(GetProperty("Fastwiretab"),5)!=null ) {
+					test.pass("Successfully opened new Fastwire tab");
+				}
+				else {
+					test.fail("Unable to relaunch Fastwire tab");
+				}
+				Thread.sleep(10000);
+		 }
 	}
 	public static String GetProperty(String Prop) {
 		return folder+LYNXProp.getProperty(Prop);		
